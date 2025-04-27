eff99a24286b693460fc60cc0f05a1b228ca1482
giant-paw <giantpawbusiness@gmail.com>
2025-04-27 21:30:37 +0700

    merapikan halaman login dan mengganti background gambar

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -17,92 +17,121 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 237, 196, 225),
-      
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Email'),
 
-            TextFormField(
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.only(top: 20),
-                isDense: true,
-                hintText: "Email",
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Icon(Icons.email),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images.bg1.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Center(
+          child: Form(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/coba_logo.png', // Ganti logo jika mau
+                    height: 200,
+                    width: 200,
                 ),
-              ),
-            ),
 
-            Text('Password'),
-
-            // TextFormField(
-            //   decoration: InputDecoration(
-            //     contentPadding: EdgeInsets.only(top: 20),
-            //     isDense: true,
-            //     hintText: "Password",
-            //     prefixIcon: Padding(
-            //       padding: EdgeInsets.only(top: 15),
-            //       child: Icon(Icons.key),
-            //     ),
-            //   ),
-            // ),
-            ShowHidePasswordTextField(
-              controller: loginpasscontroller,
-              fontStyle: const TextStyle(fontSize: 14),
-              hintColor: const Color.fromARGB(255, 0, 0, 0),
-              visibleOffIcon: Iconsax.eye_slash,
-              visibleOnIcon: Iconsax.eye,
-
-              decoration: InputDecoration(
-                isDense: true,
-                hintText: 'Enter Your Password',
-                hintStyle: Theme.of(context).textTheme.labelMedium!.copyWith(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                ),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Icon(Icons.key),
-                ),
-              ),
-            ),
-
-            ElevatedButton(onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-            }, child: Text("Login")),
-
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Belum Memiliki Akun?',
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 7, 7, 7),
-                      ),
+                SizedBox(height: 24),
+                  Text(
+                    'SELAMAT DATANG KEMBALI',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black, // Bisa disesuaikan
                     ),
-                    TextSpan(
-                      text: ' Silahkan Daftar Disini',
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 255, 11, 11),
-                      ),
+                    textAlign: TextAlign.center,
+                  ),
+
+                TextFormField(
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: "Email",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Icon(Icons.email),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+
+                Text('Password'),
+
+                // TextFormField(
+                //   decoration: InputDecoration(
+                //     contentPadding: EdgeInsets.only(top: 20),
+                //     isDense: true,
+                //     hintText: "Password",
+                //     prefixIcon: Padding(
+                //       padding: EdgeInsets.only(top: 15),
+                //       child: Icon(Icons.key),
+                //     ),
+                //   ),
+                // ),
+                ShowHidePasswordTextField(
+                  controller: loginpasscontroller,
+                  fontStyle: const TextStyle(fontSize: 14),
+                  hintColor: const Color.fromARGB(255, 0, 0, 0),
+                  visibleOffIcon: Iconsax.eye_slash,
+                  visibleOnIcon: Iconsax.eye,
+
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: 'Enter Your Password',
+                    hintStyle: Theme.of(
+                      context,
+                    ).textTheme.labelMedium!.copyWith(
+                      color: Colors.black38,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Icon(Icons.key),
+                    ),
+                  ),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  },
+                  child: Text("Login"),
+                ),
+
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Belum Memiliki Akun?',
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 7, 7, 7),
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' Silahkan Daftar Disini',
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 255, 11, 11),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
