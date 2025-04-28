dcb65b57c5d9561a2e509d034cf8ca043b4606b5
giant-paw <giantpawbusiness@gmail.com>
2025-04-28 08:22:49 +0700

    merapikan layout register

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,145 +12,169 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
-  TextEditingController loginpasscontroller = TextEditingController();
+  bool _sembunyikanPassword = true;
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/bg1.jpg'),
+            image: AssetImage('assets/images/blur.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Center(
-          child: Form(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/images/coba_logo.png', // Ganti logo jika mau
-                  height: 200,
-                  width: 200,
-                ),
+        child: Container(
+          color: Colors.white.withOpacity(0.8),
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
 
-                SizedBox(height: 24),
-                Text(
-                  'SELAMAT DATANG KEMBALI',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black, // Bisa disesuaikan
+          child: Center(
+            child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/images/coba_logo.png',
+                    height: 200,
+                    width: 200,
                   ),
-                  textAlign: TextAlign.center,
-                ),
 
-                SizedBox(height: 24),
-
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Email",
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
+                  SizedBox(height: 24),
+                  Text(
+                    'SELAMAT DATANG KEMBALI',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
-                    filled: true,
-                    fillColor: Colors.white,
+                    textAlign: TextAlign.center,
                   ),
-                ),
 
-                SizedBox(height: 24),
+                  SizedBox(height: 24),
 
-                TextFormField(
-                  obscureText: true,
-
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    prefixIcon: Icon(Icons.key),
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
                     ),
-                    fillColor: Colors.white,
                   ),
-                ),
 
-                // ShowHidePasswordTextField(
-                //   controller: loginpasscontroller,
-                //   fontStyle: const TextStyle(fontSize: 14),
-                //   hintColor: const Color.fromARGB(255, 0, 0, 0),
-                //   visibleOffIcon: Iconsax.eye_slash,
-                //   visibleOnIcon: Iconsax.eye,
+                  SizedBox(height: 24),
 
-                //   decoration: InputDecoration(
-                //     isDense: true,
-                //     hintText: 'Enter Your Password',
-                //     hintStyle: Theme.of(
-                //       context,
-                //     ).textTheme.labelMedium!.copyWith(
-                //       color: Colors.black38,
-                //       fontWeight: FontWeight.w500,
-                //       fontSize: 12,
-                //     ),
-                //     prefixIcon: Padding(
-                //       padding: EdgeInsets.only(top: 15),
-                //       child: Icon(Icons.key),
-                //     ),
-                //   ),
-                // ),
-                SizedBox(height: 32),
+                  TextFormField(
+                    obscureText: _sembunyikanPassword,
 
-                SizedBox(
-                  width: double.infinity,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 254, 75, 159),
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      prefixIcon: Icon(Icons.key),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _sembunyikanPassword
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _sembunyikanPassword = !_sembunyikanPassword;
+                          });
+                        },
+                      ),
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      fillColor: Colors.white,
+                    ),
+                  ),
+
+                  // ShowHidePasswordTextField(
+                  //   controller: loginpasscontroller,
+                  //   fontStyle: const TextStyle(fontSize: 14),
+                  //   hintColor: const Color.fromARGB(255, 0, 0, 0),
+                  //   visibleOffIcon: Iconsax.eye_slash,
+                  //   visibleOnIcon: Iconsax.eye,
+
+                  //   decoration: InputDecoration(
+                  //     isDense: true,
+                  //     hintText: 'Enter Your Password',
+                  //     hintStyle: Theme.of(
+                  //       context,
+                  //     ).textTheme.labelMedium!.copyWith(
+                  //       color: Colors.black38,
+                  //       fontWeight: FontWeight.w500,
+                  //       fontSize: 12,
+                  //     ),
+                  //     prefixIcon: Padding(
+                  //       padding: EdgeInsets.only(top: 15),
+                  //       child: Icon(Icons.key),
+                  //     ),
+                  //   ),
+                  // ),
+                  SizedBox(height: 32),
+
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(
+                          255,
+                          254,
+                          75,
+                          159,
+                        ),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(16),
                         ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomePage(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
+                  ),
+
+                  SizedBox(height: 24),
+
+                  TextButton(
                     onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomePage(),
-                        ),
-                      );
+                      Navigator.pushNamed(context, '/register');
                     },
-                    child: Text("Login", style: TextStyle(color: Colors.white),),
-                  ),
-                ),
-
-                SizedBox(height: 24),
-
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/register');
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'Belum Memiliki Akun?',
-                          style: TextStyle(
-                            color: const Color.fromARGB(255, 255, 252, 252),
-                            fontWeight: FontWeight.bold
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Belum Memiliki Akun?',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        TextSpan(
-                          text: ' Silahkan Daftar Disini',
-                          style: TextStyle(
-                            color: const Color.fromARGB(255, 254, 75, 159),
-                            fontWeight: FontWeight.bold
+                          TextSpan(
+                            text: ' Silahkan Daftar Disini',
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 254, 75, 159),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/register_page.dart
+++ lib/register_page.dart
@@ -14,145 +14,184 @@ class _RegisterPageState extends State<RegisterPage> {
   TextEditingController regisPassController = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 237, 196, 225),
-      body: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Daftar Akun Baru'),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/blur.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
 
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+        child: Container(
+          color: Colors.white.withOpacity(0.8),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 40),
+                Icon(Icons.hiking, size: 100, color: Colors.black),
+
+                const SizedBox(height: 10),
+                const Text(
+                  'DAFTAR AKUN BARU',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-                filled: true,
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                hintText: "Nama Lengkap",
-                fillColor: const Color.fromARGB(134, 255, 255, 255),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Icon(Icons.person),
+                const SizedBox(height: 30),
+
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Nama Lengkap',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-            ),
 
-            Text('Email'),
+                const SizedBox(height: 5),
 
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                filled: true,
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                hintText: "Email",
-                fillColor: const Color.fromARGB(134, 255, 255, 255),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Icon(Icons.person),
-                ),
-              ),
-            ),
-
-            Text('No HP'),
-
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                filled: true,
-                hintStyle: TextStyle(color: Colors.grey[800]),
-                hintText: "No HP",
-                fillColor: const Color.fromARGB(134, 255, 255, 255),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Icon(Icons.phone),
-                ),
-              ),
-            ),
-
-            Text(''),
-
-            ShowHidePasswordTextField(
-              controller: regisPassController,
-              fontStyle: const TextStyle(fontSize: 14),
-              hintColor: const Color.fromARGB(255, 16, 16, 16),
-              visibleOffIcon: Iconsax.eye_slash,
-              visibleOnIcon: Iconsax.eye,
-
-              decoration: InputDecoration(
-                isDense: true,
-                hintText: 'Password',
-                hintStyle: Theme.of(context).textTheme.labelMedium!.copyWith(
-                  color: const Color.fromARGB(96, 255, 0, 0),
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                ),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Icon(Icons.key),
-                ),
-              ),
-            ),
-
-            Text(''),
-
-            ShowHidePasswordTextField(
-              controller: regisPassController,
-              fontStyle: const TextStyle(fontSize: 14),
-              hintColor: const Color.fromARGB(255, 16, 16, 16),
-              visibleOffIcon: Iconsax.eye_slash,
-              visibleOnIcon: Iconsax.eye,
-
-              decoration: InputDecoration(
-                isDense: true,
-                hintText: 'Password',
-                hintStyle: Theme.of(context).textTheme.labelMedium!.copyWith(
-                  color: const Color.fromARGB(96, 255, 0, 0),
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                ),
-                prefixIcon: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: Icon(Icons.key),
-                ),
-              ),
-            ),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context, '/login');
-              },
-              child: Text('Register'),
-            ),
-
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context, '/login');
-              },
-              child: RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Sudah Memiliki Akun? Silahkan ',
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 7, 7, 7),
-                      ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                    TextSpan(
-                      text: 'Login Disini',
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 255, 11, 11),
-                      ),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    hintText: "Nama Lengkap",
+                    fillColor: const Color.fromARGB(134, 255, 255, 255),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Icon(Icons.person),
                     ),
-                  ],
+                  ),
                 ),
-              ),
+
+                Text('Email'),
+
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    hintText: "Email",
+                    fillColor: const Color.fromARGB(134, 255, 255, 255),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Icon(Icons.person),
+                    ),
+                  ),
+                ),
+
+                Text('No HP'),
+
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    filled: true,
+                    hintStyle: TextStyle(color: Colors.grey[800]),
+                    hintText: "No HP",
+                    fillColor: const Color.fromARGB(134, 255, 255, 255),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Icon(Icons.phone),
+                    ),
+                  ),
+                ),
+
+                Text(''),
+
+                ShowHidePasswordTextField(
+                  controller: regisPassController,
+                  fontStyle: const TextStyle(fontSize: 14),
+                  hintColor: const Color.fromARGB(255, 16, 16, 16),
+                  visibleOffIcon: Iconsax.eye_slash,
+                  visibleOnIcon: Iconsax.eye,
+
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: 'Password',
+                    hintStyle: Theme.of(
+                      context,
+                    ).textTheme.labelMedium!.copyWith(
+                      color: const Color.fromARGB(96, 255, 0, 0),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Icon(Icons.key),
+                    ),
+                  ),
+                ),
+
+                Text(''),
+
+                ShowHidePasswordTextField(
+                  controller: regisPassController,
+                  fontStyle: const TextStyle(fontSize: 14),
+                  hintColor: const Color.fromARGB(255, 16, 16, 16),
+                  visibleOffIcon: Iconsax.eye_slash,
+                  visibleOnIcon: Iconsax.eye,
+
+                  decoration: InputDecoration(
+                    isDense: true,
+                    hintText: 'Password',
+                    hintStyle: Theme.of(
+                      context,
+                    ).textTheme.labelMedium!.copyWith(
+                      color: const Color.fromARGB(96, 255, 0, 0),
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Icon(Icons.key),
+                    ),
+                  ),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, '/login');
+                  },
+                  child: Text('Register'),
+                ),
+
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, '/login');
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Sudah Memiliki Akun? Silahkan ',
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 7, 7, 7),
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Login Disini',
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 255, 11, 11),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
