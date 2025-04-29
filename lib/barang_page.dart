b60175d8205606c3c99327f4b57b359bcf1b36e7
giant-paw <giantpawbusiness@gmail.com>
2025-04-29 18:58:41 +0700

    membuatcontroller dan map menampung harga barang

--- lib/barang_page.dart
+++ lib/barang_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class BarangPage extends StatefulWidget {
   const BarangPage({super.key});
@@ -8,6 +9,21 @@ class BarangPage extends StatefulWidget {
 }
 
 class _BarangPageState extends State<BarangPage> {
+
+  final _tanggalController = TextEditingController();
+  final _jumlahController = TextEditingController();
+
+  String? _jenisTransaksi;
+  String? _jenisBarang;
+  int? _hargaSatuan;
+
+  final Map<String, int> _barangHarga = {
+    'Carrier': 140000,
+    'Tenda': 200000,
+    'Sleeping Bag': 80000,
+    'Sepatu': 120000,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +43,71 @@ class _BarangPageState extends State<BarangPage> {
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         iconTheme: const IconThemeData(color: Colors.white),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Tanggal Transaksi"),
+              const SizedBox(height: 8),
+              TextField(
+                controller: _tanggalController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.calendar_today),
+                  hintText: "Tanggal Transaksi",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                ),
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2101),
+                  );
+                  if (pickedDate != null) {
+                    setState(() {
+                      _tanggalController.text = DateFormat('dd-MM-yyyy').format(pickedDate);
+                    });
+                  }
+                },
+              ),
+
+              const SizedBox(height: 16),
+              DropdownButtonFormField<String>(
+                value: _jenisTransaksi,
+                items: ["Masuk", "Keluar"]
+                    .map((item) => DropdownMenuItem(value: item, child: Text(item)))
+                    .toList(),
+                onChanged: (val) => setState(() => _jenisTransaksi = val),
+                decoration: InputDecoration(
+                  hintText: "Jenis Transaksi",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                ),
+              ),
+
+              const SizedBox(height: 16),
+              
+              DropdownButtonFormField<String>(
+                value: _jenisBarang,
+                items: _barangHarga.keys
+                    .map((item) => DropdownMenuItem(value: item, child: Text(item)))
+                    .toList(),
+                onChanged: (val) => setState(() {
+                  _jenisBarang = val;
+                  _hargaSatuan = _barangHarga[val];
+                }),
+                decoration: InputDecoration(
+                  hintText: "Jenis Barang",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
