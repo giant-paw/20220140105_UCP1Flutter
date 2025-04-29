17d0ac7b76ea4bc257dbdb954d1b35474f8c340b
giant-paw <giantpawbusiness@gmail.com>
2025-04-29 19:40:40 +0700

    menampilkan data dan membuat

--- lib/detail_barang.dart
+++ lib/detail_barang.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DetailBarang extends StatelessWidget {
 
@@ -21,6 +22,37 @@ class DetailBarang extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final currencyFormatter = NumberFormat.currency(
+      locale: 'id_ID',
+      symbol: 'Rp. ',
+      decimalDigits: 0,
+    );
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 233, 233, 233),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16),
+          child: Column(
+            children: [
+              const SizedBox(height: 24),
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.green.withOpacity(0.1),
+                ),
+                padding: const EdgeInsets.all(30),
+                child: const Icon(
+                  Icons.check_circle,
+                  size: 80,
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
