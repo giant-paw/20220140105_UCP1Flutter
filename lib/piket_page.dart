666bcf11254fb26bc3a4b481b685d34221b4401f
giant-paw <giantpawbusiness@gmail.com>
2025-04-29 20:34:10 +0700

    membuat tanggal dan button

--- lib/piket_page.dart
+++ lib/piket_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class PiketPage extends StatefulWidget {
   final String emailLogin;
@@ -34,6 +36,34 @@ class _PiketPageState extends State<PiketPage> {
       selectedDate = null;
       dateError = null;
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting('id_ID', '').then((_) {
+      Intl.defaultLocale = 'id_ID';
+    });
+  }
+
+  Future<void> _selectedDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        dateError = null;
+      });
+    }
+  }
+
+  String _formatTanggal(DateTime? date) {
+    if (date == null) return 'Pilih tanggal';
+    return DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(date);
   }
 
   @override
@@ -93,6 +123,110 @@ class _PiketPageState extends State<PiketPage> {
                 ),
               ),
               const SizedBox(height: 16),
+
+              const Padding(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text(
+                  'Pilih Tanggal',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+              GestureDetector(
+                onTap: () => _selectedDate(context),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Row(
+                    children: [
+                      const Icon(Icons.calendar_month, color: Colors.black),
+                      const SizedBox(width: 10),
+                      Text(
+                        selectedDate != null
+                            ? _formatTanggal(selectedDate)
+                            : 'Pilih Tanggal',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              if (dateError != null)
+                Padding(
+                  padding: const EdgeInsets.only(top: 5),
+                  child: Text(
+                    dateError!,
+                    style: const TextStyle(color: Colors.red, fontSize: 14),
+                  ),
+                ),
+              const SizedBox(height: 16),
+              const Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  'Tugas Piket',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: tugasController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 1.5,
+                          ),
+                        ),
+                        hintText: 'Tugas Piket',
+                        hintStyle: TextStyle(color: Colors.black),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Tugas tidak boleh kosong';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: _tambahDataPiket,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(184, 39, 29, 109),
+                      fixedSize: const Size(180, 60),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text(
+                      'Tambah',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                ],
+              ),
+
+              
             ],
           ),
         ),
